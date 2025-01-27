87345f94f589ecb181e58e89743bf797a5c2655f
John Ciavarella <6977828+johnciavarella@users.noreply.github.com>
2025-01-27 11:17:34 -0500

    working. added: Satitize Path for git-repo Token support

--- webserver.py
+++ webserver.py
@@ -5,19 +5,33 @@ import time
 import subprocess
 import logging
 import argparse
+import re
 
 # Set up argument parsing
-parser = argparse.ArgumentParser(description="Flask server with Git webhook integration.")
-parser.add_argument('--git-repo-dir', required=True, help="Directory where the Git repository is located. Enter the path where the repo will be added to (parent folder where the repo will be downloaded)")
-parser.add_argument('--port', type=int, default=5123, help="Port on which the Flask app will listen. Default is 5123.")
-
+parser = argparse.ArgumentParser(
+    description="Flask server with Git webhook integration.",
+    usage="webserver.py --port 8080 --security-token true --git-repo-dir /var/www/",
+    epilog="Test: curl -X POST -H \"X-Security-Token: 12345\" http://127.0.01/webhook/site")
+parser.add_argument('--git-repo-dir', 
+                    required=True, 
+                    help="Directory where the Git repository is located. Enter the path where the repo will be added to (parent folder where the repo will be downloaded)")
+parser.add_argument('--port', 
+                    type=int, 
+                    default=5123, 
+                    help="Port on which the Flask app will listen. Default is 5123.")
+parser.add_argument('--security-token', 
+                    required=True, 
+                    help="Security token required to access the webhook endpoint. Set token here.")
+parser.add_argument('--debug', 
+                    action='store_true',
+                    help="WARNING: Insecure. Will display secrets")
 args = parser.parse_args()
 
-# Define the directory where your Git repository is located
-GIT_REPO_DIR = args.git_repo_dir
-
-# Define the port on which the Flask app will listen
-LISTEN_PORT = args.port
+# Define args 
+GIT_REPO_DIR = args.git_repo_dir # Define the directory where your Git repository is located
+LISTEN_PORT = args.port # Define the port on which the Flask app will listen
+SECURITY_TOKEN = args.security_token # Define the security token
+DEBUG = args.debug
 
 # Flag to indicate if the server should restart
 should_restart = False
@@ -27,16 +41,35 @@ restart_lock = threading.Lock()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+if DEBUG:
+    logger.info(f"Expecting security token: {SECURITY_TOKEN}")
+
+# Sanitize Inputs for --git-repo-dir to add trailing /
+if not re.search(r'/$', GIT_REPO_DIR):
+    GIT_REPO_DIR += "/" # If it doesn't, add a trailing slash
+    if DEBUG: 
+        logger.debug(f"Missing trailing slash {GIT_REPO_DIR}")
+
 app = Flask(__name__)
 
 @app.route('/webhook/<path:subpath>', methods=['POST'])
 def webhook(subpath):
     global should_restart
 
+    # Check for the security token in the request headers
+    token = request.headers.get('X-Security-Token')
+    
+    if DEBUG:
+        logger.debug(f"Recieved token: {token}")
+    
+    if token != SECURITY_TOKEN:
+        logger.warning(f"Unauthorized access attempt with token: {token}")
+        return jsonify({"status": "error", "message": "Unauthorized"}), 401
+
     # Print "Received" when the webhook is hit
     logger.info(f"Received webhook for subpath: {subpath}")
     
-    FULL_PATH=GIT_REPO_DIR + subpath
+    FULL_PATH = GIT_REPO_DIR + subpath
     
     try:
         if os.path.isdir(FULL_PATH):
@@ -49,12 +82,11 @@ def webhook(subpath):
             with restart_lock:
                 should_restart = True
 
-            return jsonify({"status": "success", "message": "Git pull successfull"}), 200
+            return jsonify({"status": "success", "message": "Git pull successful"}), 200
         else:
             logger.critical(f"Directory doesn't exist: {FULL_PATH}")
-            return jsonify({"status": "error", "message": "repo doesnt exist"}), 500
+            return jsonify({"status": "error", "message": "repo doesn't exist"}), 500
             
-        
     except subprocess.CalledProcessError as e:
         # Log the error
         error_message = f"Git pull failed: {e.stderr}"
@@ -81,4 +113,5 @@ if __name__ == '__main__':
 
     # Run the Flask app on the specified port
     logger.info(f"Starting Flask server on port {LISTEN_PORT}...")
+    
     app.run(host='0.0.0.0', port=LISTEN_PORT, use_reloader=True)
